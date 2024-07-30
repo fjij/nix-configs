e614d5f9bd878d5b506c968a5efa942e4932cd78
fjij <30779570+fjij@users.noreply.github.com>
2024-07-29 23:15:52 -0400

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "My basic NixOS flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.05";
+      # Make home manager use the same version of nixpkgs as the current flake
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+    # Here, "nixos" is the system's hostname
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.willh = import ./home.nix;
+	}
+      ];
+    };
+  };
+}
---
+++ home.nix
@@ -0,0 +1,39 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "willh";
+  home.homeDirectory = "/home/willh";
+  
+  home.packages = with pkgs; [
+    # tools
+    ripgrep
+    neovim
+    git
+    git-lfs
+    tmux
+    fish
+    jq
+    rsync
+    gnupg
+    # glitter
+    bat
+    gum
+    gh
+    tldr
+    zoxide
+    fzf
+    stylua
+    # gamer
+    nethack
+  ];
+
+  programs.git = {
+    enable = true;
+    userName = "fjij";
+    userEmail = "30779570+fjij@users.noreply.github.com";
+  };
+
+  home.stateVersion = "24.05";
+
+  programs.home-manager.enable = true;
+}
