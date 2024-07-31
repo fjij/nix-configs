535b6034f864928a4acc4a4239a99f6f418f0fdd
fjij <30779570+fjij@users.noreply.github.com>
2024-07-31 12:30:28 -0400

    Add "will" to nix-darwin known users

--- systems/mac/configuration.nix
+++ systems/mac/configuration.nix
@@ -11,9 +11,14 @@
 
   nixpkgs.hostPlatform = "aarch64-darwin";
 
+  # List of users managed by nix-darwin
+  # https://github.com/LnL7/nix-darwin/issues/811
+  users.knownUsers = ["will"];
   users.users.will = {
     home = "/Users/will";
     shell = pkgs.fish;
+    # dscl . -read /Users/will UniqueID
+    uid = 501;
   };
 
   programs.fish.enable = true;
