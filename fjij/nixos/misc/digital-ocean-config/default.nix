537742473b27ef248ae6f6e28c551026810adea9
fjij <30779570+fjij@users.noreply.github.com>
2024-08-06 17:44:01 -0400

    Add DigitalOcean config comment

--- fjij/nixos/misc/digital-ocean-config/default.nix
+++ fjij/nixos/misc/digital-ocean-config/default.nix
@@ -3,6 +3,8 @@
   lib,
   ...
 }: {
+  # I got this config from the machine.
+  # See https://justinas.org/nixos-in-the-cloud-step-by-step-part-1
   imports = [
     (modulesPath + "/virtualisation/digital-ocean-config.nix")
   ];
