bd1f4d41ca1c7acd52254d01f89aaa2617c9da87
fjij <30779570+fjij@users.noreply.github.com>
2024-10-23 10:43:20 -0400

    Rename tsserver -> ts_ls

--- dotfiles/nvim/lua/plugins/lsp.lua
+++ dotfiles/nvim/lua/plugins/lsp.lua
@@ -120,7 +120,7 @@ return {
         root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc"),
       })
 
-      lspconfig.tsserver.setup({
+      lspconfig.ts_ls.setup({
         root_dir = lspconfig.util.root_pattern("package.json"),
         single_file_support = false,
       })
