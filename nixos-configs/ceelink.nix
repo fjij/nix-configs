bd976eab6a74c546eea462b7145fcda299744690
fjij <30779570+fjij@users.noreply.github.com>
2025-06-07 23:06:08 -0400

    Netfix user

--- nixos-configs/ceelink.nix
+++ nixos-configs/ceelink.nix
@@ -13,6 +13,7 @@
       hostName = "ceelink";
     };
     wifi.enable = true;
+    wifi.netfixUser.enable = true;
     tailscale.enable = true;
     admin-user.enable = true;
     openssh.enable = true;
@@ -22,12 +23,4 @@
 
   # ZFS is broken on the latest kernel, so let's disable it
   boot.supportedFilesystems.zfs = lib.mkForce false;
-
-  users.users.network-debug-user = {
-    isNormalUser = true;
-    description = "User account for network debugging";
-    extraGroups = [ "networkmanager" ];
-    home = "/home/network-debug-user";
-    password = "password";
-  };
 }
--- nixos-modules/wifi.nix
+++ nixos-modules/wifi.nix
@@ -7,12 +7,30 @@ let
   cfg = config.fjij.wifi;
 in
 {
-  options.fjij.wifi.enable = lib.mkEnableOption "Wifi";
+  options.fjij.wifi = {
+    enable = lib.mkEnableOption "Wifi";
+    netfixUser.enable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Create a special user account for fixing network issues. Not accessible via SSH.";
+    };
+    netfixUser.name = lib.mkOption {
+      type = lib.types.str;
+      default = "netfix";
+      description = "The username for the network debugging user.";
+    };
+    netfixUser.password = lib.mkOption {
+      type = lib.types.str;
+      default = "password";
+      description = "Password for the netfix user.";
+    };
+  };
 
   config = lib.mkIf cfg.enable {
     fjij.sops.enable = true;
     sops.secrets.wifi-env = { };
 
+    networking.networkmanager.enable = true;
     networking.networkmanager.ensureProfiles = {
       environmentFiles = [
         config.sops.secrets.wifi-env.path
@@ -37,5 +55,19 @@ in
         ipv6.addr-gen-mode = "stable-privacy";
       };
     };
+
+    # Netfix user
+    users.users = lib.mkIf cfg.netfixUser.enable {
+      "${cfg.netfixUser.name}" = {
+        isNormalUser = true;
+        description = "User account for network debugging";
+        extraGroups = [ "networkmanager" ];
+        home = "/home/${cfg.netfixUser.name}";
+        password = cfg.netfixUser.password;
+      };
+    };
+    services.openssh.settings.DenyUsers = lib.mkIf cfg.netfixUser.enable [
+      cfg.netfixUser.name
+    ];
   };
 }
